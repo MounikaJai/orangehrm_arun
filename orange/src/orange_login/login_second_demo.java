c2afd552d604d87dd0ab45b136e6006fe1795930
LENOVO <LENOVO@LAPTOP-52R9CGVK>
2019-10-23 12:06:27 +0530

    third commit

---
+++ orange/src/orange_login/login_second_demo.java
@@ -0,0 +1,21 @@
+package orange_login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login_second_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin ");
+		driver.findElement(By.id("txtPassword")).sendKeys("Admin ");
+		driver.findElement(By.id("btnLogin")).click();
+
+	}
+
+}
